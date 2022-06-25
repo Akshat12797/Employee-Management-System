68b6b9354f1afd240990b19820c4f9d6386b9ab1
Akshat12797 <akshatsaurabh53@gmail.com>
2022-06-25 11:24:43 +0530

    Initial commit

---
+++ src/Data/index.js
@@ -0,0 +1,29 @@
+const employeesData = [
+
+    {
+        id: 1,
+        firstName: 'Susan',
+        lastName: 'Jordan',
+        email: 'susanjordan@gmail.com',
+        salary: '45000' ,
+        date: '2019-04-11'
+    },
+
+    {
+        id: 2,
+        firstName: 'Rick',
+        lastName: 'Jordan',
+        email: 'rickjordan@gmail.com',
+        salary: '40000' ,
+        date: '2019-04-17'
+    },
+
+    {
+        id: 3,
+        firstName: 'Morgan',
+        lastName: 'Jones',
+        email: 'morganjones@gmail.com',
+        salary: '50000' ,
+        date: '2019-05-16'
+    },
+]
